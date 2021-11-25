bc475ae9bca42085837d057469de95e5c7aef5ea
tracy-e <tracy.cpp@gmail.com>
2021-11-25 20:11:25 +0800

    background

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -2,7 +2,7 @@ import Main from '../../components/Main'
 
 const Layout = ({ children, banner = undefined }) => {
   return (
-    <div className="z-0 flex items-center w-full h-screen bg-black bg-opacity-50">
+    <div className="z-0 flex items-center w-full h-screen">
       <Main>{children}</Main>
     </div>
   )
